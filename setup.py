14bcf06ba51f44c0c2a266b5da8867a93e408864
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-11-08 16:53:01 +1000

    Add basic setup with requirements in case this repo grows

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+'''
+Functional brain age (FBA) example, basic setup to pull necessary libraries
+with pip install
+'''
+
+from setuptools import setup, find_packages
+import os
+import runpy
+
+# Get the current folder
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+#Minimal functionality
+requirements = [
+        'matplotlib',   # Plotting
+        'numpy',        # Numerical functions
+        'scipy'         # Scientific python and signal processing
+        'onnx',         # Performance-focused scoring engine for Open Neural Network Exchange (ONNX) models.
+        ]
+
+# Get version
+versionpath = os.path.join(cwd, 'version.py')
+version = runpy.run_path(versionpath)['__version__']
+
+setup(
+    name='fba-demo',
+    version=version,
+    author='Nathan Stevenson',
+    author_email='pmsl.academic@gmail.com',         # TODO: for testing purposes, to change 
+    description='Functional Age Brain predictions',
+    url='http://github.com/pausz/fba-example',      # TODO: update url
+    keywords=['scientific', 'ai', 'prediction'],
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements
+)
---
+++ version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+'''
+Version and license information.
+'''
+
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__ = '2022.11.04'
+__versiondate__ = '2022-11-04'
+__license__ = f' Functional Brain Age Demo {__version__} ({__versiondate__}) — © 2022 by Nathan Stevenson QIMR Berghofer'
