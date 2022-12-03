552095b51a0b48e66e03c152902d2069a4262845
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-12-03 14:53:49 +1000

    Write example

---
+++ demo.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Example on how to use FBA functions
+
+@authors: Nathan Stevenson, Kartik Iyer
+"""
+
+import argparse
+
+import fba
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--edf_filename',
+                    default='demo-data/random-noise.edf',
+                    type=str,
+                    help=''' The name of the .edf file with EEG data''')
+
+parser.add_argument('--raw_age',
+                    default=0.4,
+                    type=float,
+                    help='''Raw (Functional Brain Age) or just Age, expressed in years.''')
+
+parser.add_argument('--onnx_filename',
+                    default='demo-onnx/D1_NN_18ch_model.onnx',
+                    type=str,
+                    help=''' The name of the .onnx file with the pretrained network.''')
+
+parser.add_argument('--montage_filename',
+                    default='demo-data/default_fba_montage.txt',
+                    type=str,
+                    help=''' The name of the .txt with the desired EEG montage''')
+
+# This is the sequence of steps
+eeg_edf = fba.load_edf("demo-data/FLE14-609.edf")
+eeg_data = fba.make_montage(eeg_edf, preprocess=True)   # Uses default montage
+eeg_epoch = fba.make_data_epochs(eeg_data)
+onnx_model = fba.onnx_load_model()                      # Loads default model D1_NN_18ch_model.onnx
+fba_var = fba.onnx_estimate_fab(onnx_model, eeg_epoch)
+sub_id = 0
+centile, _, _, _ = fba.estimate_centile(parser.raw_age, fba_var, sub_id, to_plot=True) # Uses default bins, lookup table and offset
