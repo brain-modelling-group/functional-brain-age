5f931f03fc9d481451814f5897fc2bde3d5a5032
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-12-03 17:02:55 +1000

    Improve message

--- demo.py
+++ demo.py
@@ -7,12 +7,12 @@ Example on how to use FBA functions
 
 import argparse
 
-import fba as fba
+import fba
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--edf_filename',
-                    default='demo-data/random-noise.edf',
+                    default='fba/data/edf/FLE14-609.edf',
                     type=str,
                     help=''' The name of the .edf file with EEG data''')
 
@@ -22,26 +22,33 @@ parser.add_argument('--raw_age',
                     help='''Raw (Functional Brain Age) or just Age, expressed in years.''')
 
 parser.add_argument('--onnx_filename',
-                    default='demo-onnx/D1_NN_18ch_model.onnx',
+                    default='fba/data/onnx/D1_NN_18ch_model.onnx',
                     type=str,
                     help=''' The name of the .onnx file with the pretrained network.''')
 
 parser.add_argument('--montage_filename',
-                    default='demo-data/default_fba_montage.txt',
+                    default='fba/data/montages/default_fba_montage.txt',
                     type=str,
                     help=''' The name of the .txt with the desired EEG montage''')
 
-# Load, preprocess and chunk EEG data
-eeg_edf = fba.load_edf(filename="demo-data/FLE14-609.edf")
-eeg_data = fba.make_montage(eeg_edf, preprocess=True)   # Uses default montage
-eeg_epoch = fba.make_data_epochs(eeg_data)
 
-# Load pretrained nn model
-onnx_model = fba.onnx_load_model()                      # Loads default model D1_NN_18ch_model.onnx
+if __name__ == '__main__':
+    args = parser.parse_args()
 
-# Estimate FBA
-fba_var = fba.onnx_estimate_fab(onnx_model, eeg_epoch)
+    # Load, preprocess and chunk EEG data
+    eeg_edf = fba.load_edf(args.edf_filename)
+    eeg_data = fba.make_montage(eeg_edf, preprocess=True)   # Uses default montage
+    eeg_epoch = fba.make_data_epochs(eeg_data)
 
-# Estimate centile
-sub_id = 0
-centile, _, _, _ = fba.estimate_centile(parser.raw_age, fba_var, sub_id, to_plot=True) # Uses default bins, lookup table and offset
+    # Load pretrained nn model
+    onnx_model = fba.onnx_load_model()                      # Loads default model D1_NN_18ch_model.onnx
+
+    # Estimate FBA
+    fba_var = fba.onnx_estimate_fba(onnx_model, eeg_epoch)
+
+    # Estimate centile
+    sub_id = 0
+    centile, _, _, _ = fba.estimate_centile(args.raw_age, fba_var, sub_id, to_plot=True) # Uses default bins, lookup table and offset
+
+    msg = f"Predicted Functional Brain Age (FBA) is {fba_var} years"
+    print(msg)
