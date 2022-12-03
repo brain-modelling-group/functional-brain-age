90c92fb64472f156f06cbb8cacc56d7e22c508d2
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-12-03 15:16:01 +1000

    Document init

--- fba/__init__.py
+++ fba/__init__.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 """
 Functional Brain Age root module
+This init is here so we can access the
+functions in fba.py without having to do
+
+either:
+import fba.fba
+
+or:
+import fba.fba as fba
+
+We simply do:
+import fba as fba
+
+or (less preferred):
+from fba import *
 """
 
 from .fba import *
