4bcc4f5cb240553f1dfc6cbba5190eb7b9f41e9c
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2022-12-03 15:02:21 +1000

    Update

--- version.py
+++ version.py
@@ -6,6 +6,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '2022.11.04'
-__versiondate__ = '2022-11-04'
+__version__ = '2022.12.04'
+__versiondate__ = '2022-12-04'
 __license__ = f' Functional Brain Age Demo {__version__} ({__versiondate__}) — © 2022 by Nathan Stevenson QIMR Berghofer'
